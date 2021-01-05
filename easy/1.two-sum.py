709ff4954772c4d18aee3097b90bfb1c7ac52a0b
zhiwen liang <zhiwen_liang@outlook.com>
2021-01-05 21:26:34 +0800

    two sum and reverse integer

---
+++ easy/1.two-sum.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+
+class Solution1:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for i in range(len(nums)):
+            if target - nums[i] in hash.keys():
+                return [hash.get(target - nums[i]), i]
+            hash[nums[i]] = i
+        return []
+
+
+# test case
+nums = [2, 2, 11, 15]
+target = 9
+s = Solution1()
+print(s.twoSum(nums, target))
---
+++ easy/7.reverse-integer.py
@@ -0,0 +1,33 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        digit = len(str(abs(x)))
+        tmp = 0
+        for i in range(digit):
+            tmp = tmp + (abs(x) // 10 ** (i) % 10) * 10 ** (digit - i - 1)
+        tmp = tmp if x >= 0 else -1 * tmp
+        if tmp > 2 ** 31 - 1 or tmp < -2 ** 31:
+            return 0
+        return tmp
+
+
+class Solution1:
+    def reverse(self, x: int) -> int:
+        digit = len(str(abs(x)))
+        tmp = ''
+        tmp1 = str(abs(x))
+        for i in range(digit):
+            tmp = tmp + tmp1[digit - i - 1]
+        tmp = int(tmp) if x >= 0 else -1 * int(tmp)
+        if tmp > 2 ** 31 - 1 or tmp < -2 ** 31:
+            return 0
+        return tmp
+
+class Solution2:
+    def reverse(self, x):
+        s = cmp(x, 0)
+        r = int(repr(s*x)[::-1])
+        return s*r * (r < 2**31)
+
+# test case
+s = Solution2()
+print(s.reverse(0))
