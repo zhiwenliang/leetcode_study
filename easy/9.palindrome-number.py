a5d7729433d65fea0059d61ddccc14f7d7ba74ff
zhiwen <zhiwen_liang@outlook.com>
2021-01-06 12:58:20 +0800

    palindrome number

--- easy/9.palindrome-number.py
+++ easy/9.palindrome-number.py
@@ -10,7 +10,52 @@ class Solution:
             return True
         return False
 
+
+class SolutionA:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        tmp = 1
+        digit = 1
+        while x / tmp >= 10:
+            tmp *=10
+            digit += 1
+        for i in range(digit // 2 + digit % 2):
+            n1 = x // 10 ** i % 10
+            n2 = x // 10 ** (digit - i - 1) % 10
+            if n1 != n2:
+                return False
+        return True
+
+
+class Solution1:
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        ranger = 1
+        while x // ranger >= 10:
+            ranger *= 10
+        while x:
+            left = x // ranger
+            right = x % 10
+            if left != right:
+                return False
+            x = (x % ranger) // 10
+            ranger /= 100
+        return True
+
+
+class Solution2:
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        tmp = str(x)
+        if tmp == tmp[::-1]:
+            return True
+        return False
+
+
 # test case
-s = Solution()
-x = 0
+s = Solution1()
+x = 12
 print(s.isPalindrome(x))
