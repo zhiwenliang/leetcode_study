4fe52208f4cd228e4ebac6d97d557cfefa08a2fb
zhiwen <zhiwen_liang@outlook.com>
2021-01-08 20:16:28 +0800

    14.longest common prefix

---
+++ easy/14.longest-common-prefix.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        len1 = len(strs[0])
+        if not strs:
+            return ""
+        for i in range(len1):
+            tmp = strs[0][:i+1]
+            for j in strs:
+                tmp1 = j[:i+1]
+                if tmp != tmp1:
+                    return strs[0][:i]
+        return strs[0]
+            
+                    
+solution = Solution()
+strs = ['asfe','asdf']
+print(solution.longestCommonPrefix(strs))
