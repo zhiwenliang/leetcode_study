4fe240c8141bd297b82233aa408c159200c7f7f1
zhiwen <zhiwen_liang@outlook.com>
2021-01-15 18:09:50 +0800

    lint fix

--- easy/13.roman-to-integer.py
+++ easy/13.roman-to-integer.py
@@ -21,8 +21,8 @@ class Solution:
 
 class Solution1:
     def romanToInt(self, s: str) -> int:
-        translations = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
-                        "D": 500, "M": 1000}
+        translations = {"I": 1, "V": 5, "X": 10, "L": 50,
+                        "C": 100, "D": 500, "M": 1000}
         number = 0
         s = s.replace("IV", "IIII").replace("IX", "VIIII")
         s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
